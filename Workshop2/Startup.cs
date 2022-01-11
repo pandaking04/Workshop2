0eb4149b23e792f915bd4ac601ac63d180a2e8ee
Lenovo <khattiyatungklang@gmail.com>
2022-01-11 11:42:42 +0700

    Add project files.

---
+++ Workshop2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Workshop2.Startup))]
+namespace Workshop2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
